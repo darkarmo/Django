e491ffaad7253e53a21ace538289659d733854b4
Armo <115414955+darkarmo@users.noreply.github.com>
2024-08-06 11:11:10 +0545

    Blank/Null field

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from myapp.models import Student
+from myapp.models import Student, Customer
 admin.site.register(Student)
+admin.site.register(Customer)
 
 # Register your models here.
--- myapp/models.py
+++ myapp/models.py
@@ -10,3 +10,11 @@ class Student(models.Model):
     def __str__(self):
         return self.name
         
+class Customer(models.Model):
+    name=models.CharField(max_length=100)
+    phone_num=models.CharField(max_length=10)
+    address=models.CharField(max_length=255)
+    email=models.EmailField(max_length=254,null=True,blank=True)
+    
+    def __str__(self):
+        return self.name
