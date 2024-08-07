dbdceeabaa84179756ae208fcb01686cf8824415
Armo <115414955+darkarmo@users.noreply.github.com>
2024-08-07 11:19:19 +0545

    dictionary in html

--- project/urls.py
+++ project/urls.py
@@ -17,8 +17,17 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.shortcuts import HttpResponse,render
+from random import randint
 def index_view(request):
-    return render(request,"index.html")
+    
+    random_number=randint(1,100)
+    
+    data={"name":"John Doe",
+          "hobbies":["Singing","Dancing","Running"],
+          "random_num":random_number,
+          "is_adult":True,}
+    
+    return render(request,"index.html",data)
     #return HttpResponse("<h1>This is index page</h1>")
 def login(request):
     return render(request,"login.html",{"name":"Hyu Jack","age":"100"})
